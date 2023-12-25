93ea5f664a110601ac72c7be0a0c7cec4bcc573f
NatUsik6 <tashausik6@gmail.com>
2023-12-25 18:08:59 +0300

    feat: complete queue and stack tasks

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -14,20 +14,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.root = null;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    return this.root;
+  }
+
+  enqueue(value) {
+    if (this.root === null) {
+      this.root = new ListNode(value);
+      return;
+    }
+
+    let node = this.root;
+
+    while (node.next !== null) {
+      node = node.next;
+    }
+
+    node.next = new ListNode(value);
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let value = this.root.value;
+    this.root = this.root.next;
+    
+    return value;
   }
 }
 
